15a32a61d14c72236bbfa3a836fa4a7ffa7aab16
Jonathan Ruiz Santos <jruizs1800@alumno.ipn.mx>
2022-08-25 00:26:01 -0500

    Version2

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,18 +29,17 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.Number = new System.Windows.Forms.Label();
-            this.lbEcuations = new System.Windows.Forms.ListBox();
-            this.tbNoEcuations = new System.Windows.Forms.TextBox();
             this.tbEcuations = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
-            this.btAdd = new System.Windows.Forms.Button();
             this.btCalcular = new System.Windows.Forms.Button();
             this.tbX = new System.Windows.Forms.TextBox();
             this.label6 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbDer = new System.Windows.Forms.TextBox();
+            this.lbtest = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // label1
@@ -48,33 +47,22 @@
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(24, 14);
+            this.label1.Location = new System.Drawing.Point(90, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(346, 41);
             this.label1.TabIndex = 0;
             this.label1.Text = "Sistemas de ecuaciones";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.BackColor = System.Drawing.Color.Transparent;
-            this.label2.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label2.Location = new System.Drawing.Point(18, 71);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(200, 23);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Numero de ecuaciones :";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.BackColor = System.Drawing.Color.Transparent;
             this.label3.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label3.Location = new System.Drawing.Point(19, 107);
+            this.label3.Location = new System.Drawing.Point(24, 72);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(79, 23);
+            this.label3.Size = new System.Drawing.Size(89, 23);
             this.label3.TabIndex = 2;
-            this.label3.Text = "Ecuacion";
+            this.label3.Text = "Ecuacion :";
             // 
             // label4
             // 
@@ -89,36 +77,17 @@
             // 
             this.Number.AutoSize = true;
             this.Number.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.Number.Location = new System.Drawing.Point(93, 107);
+            this.Number.Location = new System.Drawing.Point(103, 72);
             this.Number.Name = "Number";
-            this.Number.Size = new System.Drawing.Size(20, 23);
+            this.Number.Size = new System.Drawing.Size(0, 23);
             this.Number.TabIndex = 4;
-            this.Number.Text = "0";
-            // 
-            // lbEcuations
-            // 
-            this.lbEcuations.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.lbEcuations.FormattingEnabled = true;
-            this.lbEcuations.ItemHeight = 23;
-            this.lbEcuations.Location = new System.Drawing.Point(24, 143);
-            this.lbEcuations.Name = "lbEcuations";
-            this.lbEcuations.Size = new System.Drawing.Size(346, 96);
-            this.lbEcuations.TabIndex = 5;
-            // 
-            // tbNoEcuations
-            // 
-            this.tbNoEcuations.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.tbNoEcuations.Location = new System.Drawing.Point(219, 68);
-            this.tbNoEcuations.Name = "tbNoEcuations";
-            this.tbNoEcuations.Size = new System.Drawing.Size(151, 30);
-            this.tbNoEcuations.TabIndex = 6;
             // 
             // tbEcuations
             // 
             this.tbEcuations.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.tbEcuations.Location = new System.Drawing.Point(137, 104);
+            this.tbEcuations.Location = new System.Drawing.Point(111, 69);
             this.tbEcuations.Name = "tbEcuations";
-            this.tbEcuations.Size = new System.Drawing.Size(233, 30);
+            this.tbEcuations.Size = new System.Drawing.Size(162, 30);
             this.tbEcuations.TabIndex = 7;
             // 
             // label5
@@ -127,39 +96,28 @@
             this.label5.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.label5.Location = new System.Drawing.Point(111, 107);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(15, 23);
+            this.label5.Size = new System.Drawing.Size(0, 23);
             this.label5.TabIndex = 8;
-            this.label5.Text = ":";
-            // 
-            // btAdd
-            // 
-            this.btAdd.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
-            this.btAdd.Font = new System.Drawing.Font("Segoe UI", 10.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
-            this.btAdd.Location = new System.Drawing.Point(111, 292);
-            this.btAdd.Name = "btAdd";
-            this.btAdd.Size = new System.Drawing.Size(154, 29);
-            this.btAdd.TabIndex = 9;
-            this.btAdd.Text = "Añadir Ecuacion";
-            this.btAdd.UseVisualStyleBackColor = false;
             // 
             // btCalcular
             // 
             this.btCalcular.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
             this.btCalcular.Font = new System.Drawing.Font("Segoe UI", 10.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
-            this.btCalcular.Location = new System.Drawing.Point(276, 292);
+            this.btCalcular.Location = new System.Drawing.Point(374, 143);
             this.btCalcular.Name = "btCalcular";
             this.btCalcular.Size = new System.Drawing.Size(94, 29);
             this.btCalcular.TabIndex = 10;
             this.btCalcular.Text = "Calcular";
             this.btCalcular.UseVisualStyleBackColor = false;
+            this.btCalcular.Click += new System.EventHandler(this.btCalcular_Click);
             // 
             // tbX
             // 
             this.tbX.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.tbX.Location = new System.Drawing.Point(73, 247);
+            this.tbX.Location = new System.Drawing.Point(312, 105);
             this.tbX.Name = "tbX";
             this.tbX.ReadOnly = true;
-            this.tbX.Size = new System.Drawing.Size(297, 30);
+            this.tbX.Size = new System.Drawing.Size(162, 30);
             this.tbX.TabIndex = 11;
             // 
             // label6
@@ -167,11 +125,39 @@
             this.label6.AutoSize = true;
             this.label6.BackColor = System.Drawing.Color.Transparent;
             this.label6.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label6.Location = new System.Drawing.Point(24, 250);
+            this.label6.Location = new System.Drawing.Point(263, 108);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(43, 23);
             this.label6.TabIndex = 12;
             this.label6.Text = "X = ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(274, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 23);
+            this.label2.TabIndex = 13;
+            this.label2.Text = " = ";
+            // 
+            // tbDer
+            // 
+            this.tbDer.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.tbDer.Location = new System.Drawing.Point(312, 69);
+            this.tbDer.Name = "tbDer";
+            this.tbDer.Size = new System.Drawing.Size(162, 30);
+            this.tbDer.TabIndex = 14;
+            // 
+            // lbtest
+            // 
+            this.lbtest.FormattingEnabled = true;
+            this.lbtest.ItemHeight = 20;
+            this.lbtest.Location = new System.Drawing.Point(12, 164);
+            this.lbtest.Name = "lbtest";
+            this.lbtest.Size = new System.Drawing.Size(318, 104);
+            this.lbtest.TabIndex = 15;
             // 
             // Form1
             // 
@@ -179,19 +165,18 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::SistemasEcuaciones.Properties.Resources.Fondo2;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(380, 336);
+            this.ClientSize = new System.Drawing.Size(480, 308);
+            this.Controls.Add(this.lbtest);
+            this.Controls.Add(this.tbDer);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.tbX);
             this.Controls.Add(this.btCalcular);
-            this.Controls.Add(this.btAdd);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.tbEcuations);
-            this.Controls.Add(this.tbNoEcuations);
-            this.Controls.Add(this.lbEcuations);
             this.Controls.Add(this.Number);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.MaximizeBox = false;
             this.Name = "Form1";
@@ -204,17 +189,16 @@
         #endregion
 
         private Label label1;
-        private Label label2;
         private Label label3;
         private Label label4;
         private Label Number;
-        private ListBox lbEcuations;
-        private TextBox tbNoEcuations;
         private TextBox tbEcuations;
         private Label label5;
-        private Button btAdd;
         private Button btCalcular;
         private TextBox tbX;
         private Label label6;
+        private Label label2;
+        private TextBox tbDer;
+        private ListBox lbtest;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -2,9 +2,126 @@ namespace SistemasEcuaciones
 {
     public partial class Form1 : Form
     {
+        string EcIzq;
+        string EcDer;
+        char caracter;
+        string signo;
+        string signo_menos_dos;
+        string signoint;
+        double Num1;
+        double Num2;
+        double result_suma;
+        double result_resta;
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void btCalcular_Click(object sender, EventArgs e)
+        {
+            Enter_datas();
+            string[] caract_izq_array = new string[EcIzq.Length];
+            EcIzq = EcIzq.ToUpper();
+
+           
+            for(int i = 0; i < EcIzq.Length; i++)
+            {
+                caracter = EcIzq[i];
+               
+                caract_izq_array[i] = caracter.ToString();
+
+            }
+
+            string[] list2 = new string[EcIzq.Length];
+
+            for (int j = 0; j < caract_izq_array.Length; j++)
+            {
+
+                try {
+                signo= caract_izq_array[j];
+                }
+                catch
+                {
+                    signo = "+";
+                }
+                try { 
+                signo_menos_dos = caract_izq_array[j-2];
+                }
+                catch
+                {
+                    signo_menos_dos = "0";
+                }
+                try
+                {
+                    signoint = caract_izq_array[j - 1];
+                }
+                catch
+                {
+                    signoint = "X";
+                }
+                //SUMA
+                if (signo == "+" && signoint=="X" && (signo_menos_dos!="X" || signo_menos_dos!="+" || signo_menos_dos!="-"))
+                {
+                    list2[j] = signo_menos_dos;
+       
+                }
+        
+                for(int i = 0; i < list2.Length; i++)
+                {
+                    
+                    if(i == 0)
+                    {
+                        result_suma = Convert.ToDouble(list2[i]);
+                        
+                    }
+                    else if(i != 0)
+                    {
+                        result_suma = result_suma + Convert.ToDouble(list2[i]);
+                    }
+
+                }
+
+                //RESTA
+                if (signo == "-" && signoint == "X" && (signo_menos_dos != "X" || signo_menos_dos != "+" || signo_menos_dos != "-"))
+                {
+                    list2[j] = signo_menos_dos;
+                }
+
+                for (int i = 0; i < list2.Length; i++)
+                {
+
+                    if (caract_izq_array[0] != "X" || caract_izq_array[0] != "+" || caract_izq_array[0] != "-")
+                    {
+                        list2[0] = "0";
+                    }
+                    else if (caract_izq_array[0] == "X")
+                    {
+                        list2[0] = "0";
+                    }
+
+                    if (i == 0)
+                    {
+                        result_resta = Convert.ToDouble(list2[i]);
+                        //tbX.Text = result_resta.ToString();
+
+                    }
+                    else if (i != 0)
+                    {
+                        result_resta = result_resta - Convert.ToDouble(list2[i]);
+                        //lbtest.Items.Add(list2[i]);
+                    }
+
+                    tbX.Text = result_resta.ToString();
+                }
+
+            }
+            
+        }
+        public void Enter_datas()
+        {
+            EcIzq = tbEcuations.Text;
+            EcDer = tbDer.Text;
+        }
+
     }
 }
